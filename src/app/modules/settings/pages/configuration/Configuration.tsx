fc3fad93fc7ab76e112fd3b875878b6327f573cf
shyam-kathiriya <shyam.kathiriya@saeculumsolutions.com>
2022-08-23 18:38:02 +0530

    configuration screen api integration

--- src/app/modules/settings/pages/configuration/Configuration.tsx
+++ src/app/modules/settings/pages/configuration/Configuration.tsx
@@ -1,21 +1,109 @@
-import React, { ChangeEvent, FormEvent, useState } from 'react'
+import React, { ChangeEvent, FormEvent, useEffect, useRef, useState } from 'react'
+import { useLayout } from '../../../../../_metronic/layout/core';
+import { addPayoutMarginApi, getPayoutMarginApi } from '../../../../api';
+import ConfirmationModal from '../../../../components/modal/ConfirmationModal';
+import { useAuth } from '../../../auth';
 
 const Configuration = () => {
 
-  const [margin, setMargin] = useState<string | ''>('10');
-  const [finalMargin, setFinalMargin] = useState<string | ''>('10');
+  const { auth } = useAuth();
+  const { setLoader } = useLayout();
+  const [margin, setMargin] = useState<string | undefined>(undefined);
+  const [currentMargin, setCurrentMargin] = useState<string | ''>('');
+  const [confirmationOpen, setConfirmationOpen] = useState<boolean>(false)
+  const [confirmationInfo, setConfirmationInfo] = useState<any>()
+  const ref = useRef(false)
+
+  useEffect(() => {
+    if (!ref.current) {
+      ref.current = true
+      return
+    }
+    if (!confirmationOpen) {
+      setConfirmationOpen(!confirmationOpen)
+    }
+  }, [confirmationInfo])
+
+  const getPayoutMargin = async () => {
+    setLoader(true)
+    const response = await getPayoutMarginApi({ token: auth?.token });
+    if (response && response.status === 200) {
+      setCurrentMargin(response.data.payout_margin);
+      setLoader(false)
+    }
+  }
+
+  useEffect(() => {
+    getPayoutMargin();
+  }, [])
 
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
     setMargin(e.target.value)
   }
 
-  const onSubmit = () => {
-    setFinalMargin(margin);
-    setMargin('')
+
+  const confirmationCallback = (success: boolean, info: any) => {
+    setConfirmationOpen(false)
+    if (success && info.action === 'confirmation') {
+      submitForm(confirmationInfo);
+    } else if (info.action === 'alert') {
+      setConfirmationOpen(!confirmationOpen);
+      getPayoutMargin();
+    } else if (info.action === 'error') {
+      setConfirmationOpen(false)
+    }
+  }
+
+
+  const toggleModal = (info?: any) => {
+    console.log(confirmationOpen, 'in toggle')
+    setConfirmationInfo(info);
+    setConfirmationOpen(!confirmationOpen);
+  }
+
+  const submitForm = async (info: any) => {
+    setLoader(true)
+    let payload = {
+      "payout_margin": margin
+    }
+    const response = await addPayoutMarginApi({ token: auth?.token, payload });
+    if (response && response.status === 200) {
+      setMargin('');
+      const info = { action: 'alert', message: 'Payout margin successfully added' }
+      toggleModal(info);
+      return
+    } else {
+      const info = { action: 'error', message: response.message };
+      toggleModal(info);
+    }
+  }
+
+
+  const handleSubmit = async () => {
+    let info;
+    if (!margin) {
+      info = {
+        action: 'error',
+        message: 'Please enter payout margin',
+      }
+      toggleModal(info);
+    } else {
+      info = {
+        action: 'confirmation',
+        message: 'Are You Sure To Save?',
+      }
+      toggleModal(info);
+    }
   }
 
   return (
     <>
+      {confirmationOpen && <ConfirmationModal
+        open={confirmationOpen}
+        confirmationInfo={confirmationInfo}
+        onClose={() => { setConfirmationOpen(false) }}
+        handleConfirmationMessage={confirmationCallback}
+      />}
       <div className='d-flex justify-content-between align-items-center mb-5'>
         <h1 className='fs-2hx fw-bold text-dark mb-0'>Configuration</h1>
       </div>
@@ -33,13 +121,13 @@ const Configuration = () => {
               />
             </div>
             <div>
-              <button type="button" className='btn btn-secondary' onClick={onSubmit}>Save</button>
+              <button type="button" className='btn btn-secondary' onClick={handleSubmit}>Save</button>
             </div>
           </form>
 
           <div className='d-flex align-items-center gap-4'>
             <h3 className='text-dark mb-0'>Current Payout Margin: </h3>
-            <h2 className='mb-0 fw-bold'> {finalMargin}%</h2>
+            <h2 className='mb-0 fw-bold'> {currentMargin}%</h2>
           </div>
         </div>
       </div>
