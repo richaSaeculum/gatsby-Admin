163e891f2f89849f9e31ace471f076043c6d2775
Shyam Kathiriya <91935832+shyam-kathiriya@users.noreply.github.com>
2022-12-22 15:46:15 +0530

    change api url

--- src/app/constants/configuration.ts
+++ src/app/constants/configuration.ts
@@ -1,5 +1,8 @@
 // export const apiBaseUrl = 'http://13.232.236.62:9000/api';
 // export const apiFileUploadBaseUrl = 'http://13.232.236.62:9000/api/file';
 
-export const apiBaseUrl = 'http://52.22.62.85:9030/api';
-export const apiFileUploadBaseUrl = 'http://52.22.62.85:9030/api/file';
+// export const apiBaseUrl = 'http://52.22.62.85:9030/api';
+// export const apiFileUploadBaseUrl = 'http://52.22.62.85:9030/api/file';
+
+export const apiBaseUrl = 'https://gatsby-api.onrender.com/api';
+export const apiFileUploadBaseUrl = 'https://gatsby-api.onrender.com/api/file';
